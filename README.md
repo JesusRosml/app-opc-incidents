1b62b216f281222798fc7704a0e7ba86ecb5c852
JesusRosml <jesusperezhidalgo16@outlook.com>
2024-12-19 14:33:46 -0600

    Instalación de Tailwind, React, TypeScript y React Router

---
+++ README.md
@@ -0,0 +1,1 @@
+# app-opc-incidents
---
+++ client/src/AppIncidents.tsx
@@ -0,0 +1,7 @@
+export const AppIncidents = () => {
+  return (
+    <main>
+      <h1 className="text-2xl text-blue-700">Authentication</h1>
+    </main>
+  );
+}
