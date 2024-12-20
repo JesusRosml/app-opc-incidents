cece9e03e5882889bbf2aa5d07d8465090ab55fd
JesusRosml <jesusperezhidalgo16@outlook.com>
2024-12-20 12:44:37 -0600

    Creación de las rutas de la aplicación

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { AppIncidents } from './AppIncidents'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { AppIncidents } from './AppIncidents';
+import { BrowserRouter } from 'react-router';
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppIncidents />
+    <BrowserRouter>
+      <AppIncidents />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
